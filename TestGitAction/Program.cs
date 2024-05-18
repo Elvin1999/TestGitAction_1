dba825088de4bde18135c64aff7de3eb05fd6b1c
camalzadeelvin <camalzadeelvin@STHQ0118-01>
2024-05-18 10:23:41 +0400

    added changes

--- TestGitAction/Program.cs
+++ TestGitAction/Program.cs
@@ -18,9 +18,9 @@ namespace TestGitAction
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            //some codes changes
+                              app.UseHttpsRedirection();
+                              app.UseStaticFiles();
 
 
 
